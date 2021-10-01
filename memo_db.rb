90727401f70e2157abd7287bd004af84d191e766
keikosmile <69710419+keikosmile@users.noreply.github.com>
2021-10-01 13:36:38 +0900

    テーブル名をMemosからmemosに変更。

--- memo_db.rb
+++ memo_db.rb
@@ -12,7 +12,7 @@ class MemoDB
   end
 
   def create_table
-    sql = 'CREATE TABLE IF NOT EXISTS Memos
+    sql = 'CREATE TABLE IF NOT EXISTS memos
           ( id SERIAL,
             title   VARCHAR(30)  NOT NULL,
             body    VARCHAR(500),
@@ -22,14 +22,14 @@ class MemoDB
   end
 
   def select(id)
-    sql = 'SELECT title, body FROM Memos WHERE id = $1'
+    sql = 'SELECT title, body FROM memos WHERE id = $1'
     result = @conn.exec_params(sql, [id])
     result[0]
   end
 
   def select_all
     memos = {}
-    sql = 'SELECT * FROM Memos ORDER BY id'
+    sql = 'SELECT * FROM memos ORDER BY id'
     result = @conn.exec(sql)
     result.each do |tuple|
       memos[tuple['id']] = { 'title' => tuple['title'], 'body' => tuple['body'] }
@@ -38,19 +38,19 @@ class MemoDB
   end
 
   def insert(title, body)
-    sql = 'INSERT INTO Memos (title, body) VALUES ($1, $2)'
+    sql = 'INSERT INTO memos (title, body) VALUES ($1, $2)'
     deallocate_statement
     @conn.prepare('statement', sql)
     @conn.exec_prepared('statement', [title, body])
   end
 
   def delete(id)
-    sql = 'DELETE FROM Memos WHERE id = $1'
+    sql = 'DELETE FROM memos WHERE id = $1'
     @conn.exec_params(sql, [id])
   end
 
   def update(id, title, body)
-    sql = 'UPDATE Memos SET (title, body) = ($1, $2) WHERE id = $3'
+    sql = 'UPDATE memos SET (title, body) = ($1, $2) WHERE id = $3'
     deallocate_statement
     @conn.prepare('statement', sql)
     @conn.exec_prepared('statement', [title, body, id])
